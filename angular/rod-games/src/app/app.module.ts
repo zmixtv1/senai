2fd53f2e93cd825476d5eb4751cb76adfaed06fd
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2024-02-26 16:02:03 -0300

    1.0.2

--- angular/rod-games/src/app/app.module.ts
+++ angular/rod-games/src/app/app.module.ts
@@ -21,6 +21,11 @@ import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestritoComponent } from './restrito/restrito.component';
+import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
+import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
+import { MenuProdutoComponent } from './restrito/menu-produto/menu-produto.component';
 
 
 @NgModule({
@@ -29,7 +34,12 @@ import { RodapeComponent } from './rodape/rodape.component';
     InicioComponent,
     LoginComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    RestritoComponent,
+    AtualizaProdutoComponent,
+    CadastroProdutoComponent,
+    ListaProdutoComponent,
+    MenuProdutoComponent
   ],
   imports: [
     BrowserModule,
