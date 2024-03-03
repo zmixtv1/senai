a9057654904ee6b160f7136608abb87c946d0430
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2024-03-02 22:08:40 -0300

    Update produto.service.ts

--- angular/rod-games/src/app/produto.service.ts
+++ angular/rod-games/src/app/produto.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from './models/Produto.model';
+import { throwToolbarMixedModesError } from '@angular/material/toolbar';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +12,27 @@ export class ProdutoService {
   private url = "http://localhost:3000/produtos";
   constructor(private _httpClient: HttpClient) {
     
+  }
+  getProduto(id:any): Observable<Produto>{
+    const urlIdProduto = `${this.url}?id=${id}`;
+    return this._httpClient.get<Produto>(urlIdProduto); 
+  }
+
+  getProdutos(): Observable<Produto[]>{
+    return this._httpClient.get<Produto[]>(this.url);
+  }
+  
+  cadastrarProduto(produto:Produto): Observable<Produto[]>{
+    return this._httpClient.post<Produto[]>(this.url, produto);
+  }
+
+  atualizaProduto(id:any, produto: Produto): Observable<Produto[]>{
+    const urlAtualizar = `${this.url}/?${id}`;
+    return this._httpClient.put<Produto[]>(urlAtualizar, produto);
+  } 
+
+  removerProduto(id:any): Observable<Produto[]>{
+    const urlDeletar = `${this.url}/?${id}`;
+    return this._httpClient.delete<Produto[]>(urlDeletar);
   }
 }
